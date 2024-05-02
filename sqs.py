a828c9ae5dfb047a1f81ea31a2c0b5b98ed3e5c7
Emma Cann <zgb8ts@virginia.edu>
2024-05-02 08:59:29 +0000

    Reassembling SQS Messages

--- sqs.py
+++ sqs.py
@@ -7,11 +7,16 @@ import json
 url = "https://sqs.us-east-1.amazonaws.com/440848399208/zgb8ts"
 sqs = boto3.client('sqs')
 
-def get_message():
+
+
+# Retrieve all messages
+
+def get_message(): 
+    messages = [ ]
+
     try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
+        for counter in range (0, 10, 1):
+            response = sqs.receive_message(
             QueueUrl=url,
             AttributeNames=[
                 'All'
@@ -20,28 +25,79 @@ def get_message():
             MessageAttributeNames=[
                 'All'
             ]
-        )
-        # Check if there is a message in the queue or not
-        if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
-            
+            )  
+            if "Messages" in response:
+                for i in response["Messages"]:
+                    MyMessages= {"Order": int(i['MessageAttributes']['order']['StringValue']),"Word": i['MessageAttributes']['word']['StringValue']}
+                    handle= i['ReceiptHandle']
+                    print(handle)
+                    # print(MyMessages)
+                    # messages.append(MyMessages)
+                    # Output= json.dumps(messages)
+                    # print(Output)
+                    # with open('data3.json', 'w', encoding='utf-8') as f:
+                    #     json.dump(Output, f, ensure_ascii=False)
+            else:
+                print("No message in the queue")
 
-        # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
-            
-    # Handle any errors that may occur connecting to SQS
     except ClientError as e:
         print(e.response['Error']['Message'])
 
-# Trigger the function
 if __name__ == "__main__":
     get_message()
+
+
+## Store and Sort
+
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import os
+
+MONGOPASS = os.getenv('MONGOPASS')
+uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+
+db = client.zgb8ts
+collection = db.save
+
+with open('data3.json', 'r') as f:
+    file_data = json.load(f)
+    print(file_data)
+     
+
+    if isinstance(file_data, list):
+        try:
+            collection.insert_many(file_data)
+        except Exception as e:
+            print(e, "when importing into Mongo")
+    else:
+        try:
+            collection.insert_one(file_data)
+        except Exception as e:
+            print(e)
+
+mydat= db.save.find().sort("Order", 1)
+for x in mydat:
+    print(x["Word"])
+
+
+
+
+# Delete Messages in Queue
+
+def delete_message():
+    while True:
+        response = sqs.receive_message(QueueUrl=url,AttributeNames=['All'],
+        MaxNumberOfMessages=10, MessageAttributeNames=['All'])  
+        if "Messages" in response:
+            for i in response["Messages"]:
+                sqs.delete_message_batch(
+                QueueUrl=url,
+                ReceiptHandle= i['ReceiptHandle'])
+                print("Message deleted") 
+        else:
+            print('Empty queue')  
+            break  
+
+if __name__ == "__main__":
+    delete_message()    
